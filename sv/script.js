a8bd52362b6536fe0aeebc54866ade08e87069bc
Victorlock17 <victorgamarrahuerta@gmail.com>
2026-02-14 11:12:44 -0500

    Add files via upload

---
+++ sv/script.js
@@ -0,0 +1,8 @@
+// Mostrar fecha y hora actual
+const fecha = new Date();
+document.getElementById("fecha").textContent =
+  "Fecha y hora: " + fecha.toLocaleString();
+
+function respuesta() {
+  alert("¡Sabía que dirías que sí, Jimena! ❤️");
+}
